b2ce235776ddfb6d5063eca151ecf4519ffe500d
edu <edu@mail.pl>
2023-04-01 12:52:31 +0200

    d1t2 properties and @Value examples also with default val

--- src/main/java/pl/coderslab/springbootonlaljd02/controller/HomeController.java
+++ src/main/java/pl/coderslab/springbootonlaljd02/controller/HomeController.java
@@ -1,12 +1,18 @@
 package pl.coderslab.springbootonlaljd02.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    @Value("${customer.given:Alicja}")
+    private String customerGivenGame;
+    @Value("${customer.family}")
+    private String customerFamilyGame;
+
     @GetMapping("/")
     public String home() {
-        return "welcome home!";
+        return String.format("welcome home, %s %s!", customerGivenGame, customerFamilyGame);
     }
 }
