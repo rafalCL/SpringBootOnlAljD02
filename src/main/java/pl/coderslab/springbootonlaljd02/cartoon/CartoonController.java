c2ad5e95ee7c859194d4bc89f36329450e047205
edu <edu@mail.pl>
2023-04-01 16:03:42 +0200

    d1t3z1 example API Richardson 0

---
+++ src/main/java/pl/coderslab/springbootonlaljd02/cartoon/CartoonController.java
@@ -0,0 +1,46 @@
+package pl.coderslab.springbootonlaljd02.cartoon;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CartoonController {
+    @GetMapping("/getAllCartoons")
+    public String getAllCartoons() {
+        if(Math.random() < .01) {
+            throw new RuntimeException("wystąpił wyjątek");
+        }
+
+        return "[{cartoon1},...,{cartoonN}]";
+    }
+
+    @GetMapping("/getCartoonById")
+    public String getCartoonById(@RequestParam Long id) {
+        if(Math.random() < .01) {
+            throw new RuntimeException("wystąpił wyjątek");
+        }
+
+        return "{cartoon"+id+"}";
+    }
+
+    @PostMapping("/createCartoon")
+    public String createCartoon(@RequestBody String body) {
+        if(Math.random() < .01) {
+            throw new RuntimeException("wystąpił wyjątek");
+        }
+
+        Cartoon cartoon = new Cartoon();
+        // todo handle cartoon creation
+
+        return "utworzono cartoon: "+body;
+    }
+
+    @GetMapping("/deleteCartoonById")
+    public String deleteCartoonById(@RequestParam Long id) {
+        if(Math.random() < .01) {
+            throw new RuntimeException("wystąpił wyjątek");
+        }
+        return "usunięto cartoon id="+id;
+    }
+}
