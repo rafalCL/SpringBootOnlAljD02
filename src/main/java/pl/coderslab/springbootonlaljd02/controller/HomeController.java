4b7dfcaee0cb92578955b196aa16b672e730f7e4
edu <edu@mail.pl>
2023-04-01 15:51:48 +0200

    d1t3 response status example

--- src/main/java/pl/coderslab/springbootonlaljd02/controller/HomeController.java
+++ src/main/java/pl/coderslab/springbootonlaljd02/controller/HomeController.java
@@ -1,7 +1,9 @@
 package pl.coderslab.springbootonlaljd02.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +16,20 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return String.format("welcome home, %s %s!", customerGivenGame, customerFamilyGame);
+    }
+
+    @GetMapping("/stat")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public String stat() {
+        return "methoda się powiodła";
+    }
+
+    @GetMapping("/ex")
+    public String ex() {
+        if(Math.random() < .5) {
+            throw new RuntimeException("wystąpił wyjątek");
+        }
+
+        return "methoda się powiodła";
     }
 }
