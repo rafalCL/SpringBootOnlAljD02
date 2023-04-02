2e050ce901571647d9d728944f426ea388f0781c
edu <edu@mail.pl>
2023-04-02 16:39:00 +0200

    d6t4 example 2 more example calls

---
+++ src/main/java/pl/coderslab/springbootonlaljd02/cinema/genre/GenreApiResponse.java
@@ -0,0 +1,12 @@
+package pl.coderslab.springbootonlaljd02.cinema.genre;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class GenreApiResponse {
+    private String name;
+}
--- src/main/java/pl/coderslab/springbootonlaljd02/clientservice/RestServiceClientController.java
+++ src/main/java/pl/coderslab/springbootonlaljd02/clientservice/RestServiceClientController.java
@@ -1,11 +1,14 @@
 package pl.coderslab.springbootonlaljd02.clientservice;
 
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 import pl.coderslab.springbootonlaljd02.cinema.genre.Genre;
+
+import java.util.Arrays;
 
 @Controller
 @RequestMapping("/client")
@@ -21,4 +24,45 @@ public class RestServiceClientController {
         return "wysłano zapytanie http, obiekt odpowiedzi: "+responseObj;
     }
 
+    @GetMapping("/call2")
+    @ResponseBody
+    public String exampleExternalApiCall() {
+        final String url = "https://api.chucknorris.io/jokes/random";
+        final String responseBody = new RestTemplate().getForObject(url, String.class);
+
+        return "wysłano zapytanie http, ciało odpowiedzi: " + responseBody;
+    }
+
+    @GetMapping("/call3")
+    @ResponseBody
+    public String exampleApiCall2() {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:8082/genres/{id}";
+        ResponseEntity<Genre> responseEntity = restTemplate.getForEntity(url, Genre.class, 4242L);
+        if (responseEntity.getStatusCode() == HttpStatus.OK) {
+            return "wysłano zapytanie http, obiekt odpowiedzi: "+responseEntity.getBody();
+        }
+
+        return "wysłano zapytanie http, serwis zwrócił błąd: "+responseEntity.getStatusCode() + ", " + responseEntity.getStatusCodeValue();
+    }
+
+    @GetMapping("/exchange")
+    @ResponseBody
+    public String exchangeExample() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = "http://localhost:8082/genres/{id}";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+        ResponseEntity<Genre> responseEntity = restTemplate.exchange(url, HttpMethod.GET, entity, Genre.class, 1);
+
+        if (responseEntity.getStatusCode() == HttpStatus.OK) {
+            return "wysłano zapytanie http, obiekt odpowiedzi: "+responseEntity.getBody();
+        }
+
+        return "wysłano zapytanie http, serwis zwrócił błąd: "+responseEntity.getStatusCode() + ", " + responseEntity.getStatusCodeValue();
+    }
 }
